89ee4c14fba1c14bce6ef09f592c6d266657bb04
phonghoang@gmail.com <phonghoang@gmail.com>
2015-09-15 16:18:02 -0700

    pair programming with peter and darragh

--- username.rb
+++ username.rb
@@ -2,13 +2,31 @@
 # Run `rspec /spec/username_spec.rb` to get started.
 
 def format_name(first, last)
-  nil
+  if (first === '') 
+    return nil
+  elsif (last === '')
+    return nil
+  else
+    first1 = first.gsub(/[^a-z]/i, "").downcase #if not a-z, remove from string? #
+    last1 = last.gsub(/[^a-z]/i, "").downcase
+    first1[0] + last1
+  end
 end
 
-def format_year
-  nil
+
+def format_year(year)
+  if (year.to_s.length != 4)
+    return nil
+  end
+  string = year.to_s
+  (string[2] + string[3])
 end
 
-def build_username
-  nil
+# format_year(1982)
+
+def build_username(first, last, year)
+  format_name(first, last) + format_year(year)
 end
+
+
+
